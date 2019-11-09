a3b4c988f04171624d84f1f869a775586503e388
FIRAT KARAKUYU <firatkarakuyu@firats-mac.local>
2019-11-09 09:35:28 -0800

    Initial Commit

---
+++ Homeowner/ItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemCell.swift
+//  Homeowner
+//
+//  Created by Fırat Karakuyu on 10.11.2019.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
+}
